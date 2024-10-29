3e64279162bdb53844c16c44b3f9314f1a3d4dc8
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2024-10-29 23:19:25 +0530

    Create 2Sum code.cpp

---
+++ 2Sum /2Sum code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         vector <int> sum;
+       for(int i=0;i<nums.size()-1;++i){
+        for(int j=1;j<nums.size();++j){
+            if(nums[i]+nums[j] == target && i<j){
+              sum.push_back(i);
+              sum.push_back(j);
+              break;
+        }
+        }
+       }
+        return sum;
+    }
+};
