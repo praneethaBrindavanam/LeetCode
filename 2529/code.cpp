516eee5404cb121991ec2dc0b9379edf8727f506
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-03-12 22:44:14 +0530

    Create code.cpp

---
+++ 2529/code.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int pos_count = 0, neg_count = 0; 
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) pos_count++;  
+            else if (nums[i] < 0) neg_count++; 
+        }
+        
+        return max(pos_count, neg_count); 
+    }
+};
