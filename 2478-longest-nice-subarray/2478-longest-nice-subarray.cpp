778566f1b59829178e99de4bef9fcdeb688f28d6
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-05-11 09:49:57 +0530

    Time: 0 ms (100%), Space: 60.8 MB (99.43%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int n = nums.size();
+        int maxLen = 0, left = 0, bitmask = 0;
+        for (int right = 0; right < n; ++right) {
+            while ((bitmask & nums[right]) != 0) {
+                bitmask ^= nums[left];  
+                left++;
+            }
+            bitmask |= nums[right];
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
