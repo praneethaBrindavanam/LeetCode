682c282a6ae240402572243ffe6ab78f0a67aee8
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-08-17 23:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 264 MB (58.35%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -2,9 +2,14 @@ class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
         int sum=0;
-        for(int d:derived)
-        sum^=d;
+        int n=derived.size();
+
+        for(int i=0;i<n;i++)
+            sum ^=derived[i];
         return sum==0;
+
+        
+        
         
         
     }
