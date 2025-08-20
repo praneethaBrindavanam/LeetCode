db88dd2e6cab0502795b39c0661afa4cfcc64c30
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-08-20 22:08:14 +0530

    Time: 4 ms (30.45%) | Memory: 8.3 MB (83.16%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        
+        //2 pointers
+        string result="";
+        int i=0,j=0;
+        while(i<word1.length()||j<word2.length()){
+            if(i<word1.length())
+            result.push_back(word1[i]);
+            i++;
+            if(j<word2.length())
+            result.push_back(word2[j]);
+            j++;
+
+        }
+        return result;
+
+
+
+        
+    }
+};
