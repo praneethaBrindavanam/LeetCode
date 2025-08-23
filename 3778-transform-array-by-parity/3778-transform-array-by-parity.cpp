c34b0d8e21f8ad36f40f6fa410514ee41d3ce293
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-08-23 16:12:10 +0530

    Time: 0 ms (100%), Space: 28.2 MB (61.55%) - LeetHub

---
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==0){
+                nums[j++]=0;
+            }
+        }
+        while(j<nums.size()){
+            nums[j++]=1;
+        }
+        return nums;
+
+        
+    }
+};
