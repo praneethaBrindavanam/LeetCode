de5aa864a35af8125fc35922e0334b7ed20464bf
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-09-05 00:19:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.11%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -4,7 +4,8 @@ public:
         int d1=abs(x-z);
         int d2=abs(y-z);
         if(d1<d2) return 1;
-        if(d2<d1) return 2;
+        else if(d2<d1) return 2;
+        else
         return 0;
 
         
