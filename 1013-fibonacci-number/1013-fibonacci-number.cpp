f12ebabf127fd2fe4b778783d83281c1063a9874
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-09-13 09:00:13 +0530

    Time: 9 ms (49.66%), Space: 7.8 MB (63.93%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -3,14 +3,8 @@ public:
     int fib(int n) {
         if(n <= 1) {
             return n;
-        } else {
-            int f = 0, f1 = 1, f2 = 0;
-            for(int i = 2; i <= n; i++) {
-                f2 = f + f1;
-                f = f1;
-                f1 = f2;
-            }
-            return f2;
-        }
+        } else 
+            return fib(n-1)+fib(n-2);
+        
     }
 };
