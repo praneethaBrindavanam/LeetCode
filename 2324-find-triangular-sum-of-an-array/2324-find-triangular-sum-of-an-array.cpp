86564073c1444371e642919d0a2a4056463f025c
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-09-30 08:14:35 +0530

    Time: 55 ms (74.81%), Space: 22.3 MB (74.81%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -3,10 +3,11 @@ public:
     int triangularSum(vector<int>& nums) {
         int n=nums.size();
         int left=0,right=n-1;
-            for(int size=n;size>1;size--){
-                for(int i=0;i<size-1;i++){
-                nums[i]=(nums[i]+nums[i+1])%10;
-            }
+        while(left<right){
+        for(int i=0;i<right;i++){
+            nums[i]=(nums[i]+nums[i+1])%10;
+        }
+        right--;
         }
         return nums[0];
     }
