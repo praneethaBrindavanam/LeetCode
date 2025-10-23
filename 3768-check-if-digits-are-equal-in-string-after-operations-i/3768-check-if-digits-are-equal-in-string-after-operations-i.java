02b250f259c8ef6bae77812c28a97bf69a05cca9
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-10-23 18:31:18 +0530

    Time: 1 ms (100%), Space: 41.8 MB (99.2%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,19 +1,17 @@
 class Solution {
-    public boolean hasSameDigits(String s) {
-        
-        while (s.length() > 2) {
-            StringBuilder next = new StringBuilder();
-            for (int i = 0; i < s.length() - 1; i++) {
-                
-                int sum = (s.charAt(i) - '0') + (s.charAt(i + 1) - '0');
-                next.append(sum % 10);
-            }
-            s = next.toString(); 
-        }
-        
-        return s.charAt(0) == s.charAt(1);
+  public boolean hasSameDigits(String s) {
+    int[] arr = new int[s.length()];
+
+    for (int i = 0; i < arr.length; i++)
+      arr[i] = s.charAt(i) - '0';
+    
+    for (int length = arr.length; length > 2; length--) {
+      for (int i = 0; i < length - 1; i++) {
+        arr[i] = (arr[i] + arr[i + 1]) % 10;
+      }
     }
 
-    
-    
+    return arr[0] == arr[1];
+      
+  }
 }
