77a553fcdab9a1b5e21412d62aabf64b0d47f608
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-11-06 10:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (65.67%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int sum=a+b;
-        return sum;
+        while (b != 0) {
+            int carry = (a & b) << 1;
+            a = a ^ b;   
+            b = carry;
+        }
+        return a;
     }
 };
