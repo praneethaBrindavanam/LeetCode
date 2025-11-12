d54cda1ccc2e15f7c9070c3f7a7b99df68400de4
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2025-11-12 10:44:46 +0530

    Time: 0 ms (100%), Space: 34.8 MB (51.57%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int finalPositionOfSnake(int n, vector<string>& commands) {
+        int i = 0, j = 0;
+        for(int k = 0; k < commands.size(); k++){
+            if(commands[k] == "RIGHT") j++;
+            else if(commands[k] == "LEFT") j--;
+            else if(commands[k] == "UP") i--;
+            else if(commands[k] == "DOWN") i++;
+        }
+        return (i * n) + j;
+    }
+};
