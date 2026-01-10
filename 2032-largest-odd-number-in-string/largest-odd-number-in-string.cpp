de710ec4bfcc4850781d9f394cc109c9559cc917
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2026-01-10 13:14:39 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (17.04%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        int n=num.size();
-        if(num[n-1]%2!=0)
-        return num;
-        for(int i=n-1;i>=0;i--){
-            if(num[i]%2!=0){
+        for(int i=num.size()-1;i>=0;i--){
+            if((num[i]-'0')%2!=0){
                 return num.substr(0,i+1);
             }
         }
         return "";
-
-        
     }
 };
