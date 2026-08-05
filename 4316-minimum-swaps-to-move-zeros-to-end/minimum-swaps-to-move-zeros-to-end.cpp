36367fefaf24ba10b41f46c3b2d63501adca152b
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2026-08-05 12:46:52 +0530

    Time: 1 ms (14.14%) | Memory: 31.8 MB (56.51%) - LeetSync

--- 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
+++ 4316-minimum-swaps-to-move-zeros-to-end/minimum-swaps-to-move-zeros-to-end.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minimumSwaps(vector<int>& nums) {
-        int ct=0;
-        for(int x:nums)if(x==0)ct++;
-        int j=nums.size()-1;
-        int bc=0;
-        int tmp=ct;
-        while(tmp--){
-            if(nums[j]==0)bc++;
-            j--;
-        }
-        return ct-bc;
+        int n=nums.size();
+        int count=0;
+        int zeroes=0;
+        for(int i=0;i<n;i++)
+            {
+                if(nums[i]==0)
+                zeroes++;
+            }
+        for(int i=n-zeroes;i<n;i++)
+            {
+                if(nums[i]!=0)
+                    count++;
+            }
+        
+        return count;
     }
 };
