ae1949c604dad12e27986cacd1185a1dddb6fff1
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2026-08-10 11:11:09 +0530

    Time: 15 ms (66.48%) | Memory: 44.2 MB (79.14%) - LeetSync

---
+++ 837-most-common-word/most-common-word.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String low = paragraph.toLowerCase();
+        String[] words = low.split("[^a-z]+");
+        String res = "";
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (word.isEmpty()) continue; 
+            boolean isBanned = false;
+            for (String bannedWord : banned) {
+                if (word.equals(bannedWord)) {
+                    isBanned = true;
+                    break;
+                }
+            }
+            if (!isBanned) {
+                map.put(word, map.getOrDefault(word, 0) + 1);
+            }
+        }
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                res = entry.getKey();
+            }
+        }
+        return res;
+    }
+}
