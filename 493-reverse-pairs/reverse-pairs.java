cd59453c8f69aade35bc12c2425cc649eb36b97e
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2026-09-11 23:29:31 +0530

    Time: 43 ms (72.07%) | Memory: 64.6 MB (21.02%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.java
@@ -0,0 +1,60 @@
+class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public int mergeSort(int[] nums, int low, int high) {
+
+        if (low >= high)
+            return 0;
+
+        int mid = low + (high - low) / 2;
+
+        int count = 0;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+
+        int j = mid + 1;
+
+        for (int i = low; i <= mid; i++) {
+
+            while (j <= high && (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
+
+            count += j - (mid + 1);
+        }
+
+        int left = low;
+        int right = mid + 1;
+
+        int[] temp = new int[high - low + 1];
+
+        int k = 0;
+
+        while (left <= mid && right <= high) {
+
+            if (nums[left] <= nums[right]) {
+                temp[k++] = nums[left++];
+            } else {
+                temp[k++] = nums[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[k++] = nums[left++];
+        }
+
+        while (right <= high) {
+            temp[k++] = nums[right++];
+        }
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+
+        return count;
+    }
+}
