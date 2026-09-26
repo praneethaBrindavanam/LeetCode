ce0ed4df6cf1f449be9090c3ad249d783b1e3d34
Brindavanam indira praneetha. <praneethabrindavanam04@gmail.com>
2026-09-26 23:03:27 +0530

    Time: 1 ms (95.47%) | Memory: 44.1 MB (47.56%) - LeetSync

---
+++ 4421-minimum-queen-moves-to-reach-target/minimum-queen-moves-to-reach-target.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minQueenMoves(int[] source, int[] target) {
+        int sr=source[0],sc=source[1];
+        int tr=target[0],tc=target[1];
+        if(sr==tr && sc==tc )
+        return 0;
+        if(sr==tr)
+        return 1;
+        if(sc==tc) 
+        return 1;
+        if(Math.abs(sr-tr)==Math.abs(sc-tc)) 
+        return 1;
+        return 2;
+        
+    }
+}
